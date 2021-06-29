c1c2fa05c69208d29ab03bde18607cc5f1787985
atravanam-git <praveen.atravanam@gmail.com>
2021-06-29 23:17:27 +0530

    added some comments

--- src/com/SingletonClassDemo/AccessSingleClass.java
+++ src/com/SingletonClassDemo/AccessSingleClass.java
@@ -4,9 +4,13 @@ public class AccessSingleClass {
 
 	public static void main(String[] args)
 	{
-//		object2 is assignes getinstance method
+//		object2 is assigned getInstance() method return type 
+//		which is an instance of SingletonCls
 		SingletonCls object2 = SingletonCls.getInstance();
 		object2.showMessage();
+//		The below code of crating the instance via a constructor is not possible
+//		SingletonCls object1=new SingletonCls();
+		
 
 	}
 
